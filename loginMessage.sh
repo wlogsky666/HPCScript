f219d759d5354a26a6e84a0187b4c8201ea5e072
Wlogsky <wlogsky666@gmail.com>
2017-02-19 23:58:09 +0800

    The message while you're logging

---
+++ loginMessage.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+echo '###########################################################################'
+echo '##                                                                       ##'
+echo '##                          HPC Cluseter of NTNU                         ##'
+echo '##                                                                       ##'
+echo '###########################################################################'
+
+#nodes info
+
+echo ''
+echo 'Information of computing nodes:'
+echo ''
+
+
+
+##Note
+
+echo '==========================================================================='
+echo 'Notice:'
+echo ''
+
+echo '1. Please use queueing system to submit your jobs.'
+echo '2. Please use /lustre/lwork/YOUR_ACCOUNT as your working directory.'
+echo '3. Please put your tmp files in /tmp instead of global space.'
+echo '4. Please check your job status frequently.'
+echo '5. Please clean your tmp files after job finished.'
