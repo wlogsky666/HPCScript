39797e05ab282e975c45f78b20ba662c830c3a8d
Wlogsky <wlogsky666@gmail.com>
2017-02-20 00:08:12 +0800

    Add Queues info

--- loginMessage.sh
+++ loginMessage.sh
@@ -12,7 +12,22 @@ echo ''
 echo 'Information of computing nodes:'
 echo ''
 
+##Queue info
 
+echo '==========================================================================='
+echo 'Information of Queues:'
+echo ''
+
+echo 'q24cores   :'
+echo 'q48cores   :'
+echo 'q96cores   :'
+echo 'q192cores  :'
+echo 'q384cores  :'
+echo 'bigmem128  :'
+echo 'bigmem1024 :'
+echo 'gpu        :'
+echo 'test		 :'
+echo ''
 
 ##Note
 
