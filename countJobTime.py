25d88345241d8cc45a690dae7889df8b04d30d10
Wlogsky <wlogsky666@gmail.com>
2017-03-12 22:27:18 -0500

    countJobTIme.py : count used time of finished job

---
+++ countJobTime.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+'''
+This script is used to count used time of each job
+Log files are located in /var/spool/torque/server_logs on master node
+
+'''
+
+import os
+import datetime
+import subprocess
+
+## Get date of yesterday
+## Log file will create at ?? every day
+day = datetime.date.today() - datetime.timedelta(days=1)
+
+filename = '/var/spool/torque/server_logs/' + day.strftime("%Y%m%d")
+#print('Get log file : '+filename)
+
+ssh = subprocess.Popen(['ssh', 'master', 'less '+filename+' | grep -i job'], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+for line in ssh.stdout.readlines():
+        print(line)
+        print
+
