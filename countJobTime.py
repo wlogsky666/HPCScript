46d93f1eee00951786ea1f58b3fbff3a08927c89
Wlogsky <wlogsky666@gmail.com>
2017-03-16 00:29:09 +0800

    Cut parameter 10th and 22th

--- countJobTime.py
+++ countJobTime.py
@@ -2,7 +2,7 @@
 
 '''
 This script is used to count used time of each job
-Log files are located in /var/spool/torque/server_logs on master node
+Log files are located in /var/spool/torque/server_priv/accounting/ on master node
 
 '''
 
@@ -14,10 +14,10 @@ import subprocess
 ## Log file will create at ?? every day
 day = datetime.date.today() - datetime.timedelta(days=1)
 
-filename = '/var/spool/torque/server_logs/' + day.strftime("%Y%m%d")
+filename = '/var/spool/torque/server_priv/accounting' + day.strftime("%Y%m%d")
 #print('Get log file : '+filename)
 
-ssh = subprocess.Popen(['ssh', 'master', 'less '+filename+' | grep -i job'], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+ssh = subprocess.Popen(['ssh', 'master', 'less '+filename+' | grep -e cput | cut -d" " -f10,22'], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 for line in ssh.stdout.readlines():
         print(line)
