1bdcb6affabd77f46aaa4a981dc34c3b2894ebc0
Wlogsky <wlogsky666@gmail.com>
2017-04-28 19:31:22 +0800

    Add filename

--- expireUser.py
+++ expireUser.py
@@ -3,22 +3,21 @@
 import os
 import subprocess
 
+## Get rsync file of account list at /home/wwwsync/input/
 
-'''
-Get rsync file of ban list at/home/wwwsync/input/   
+homeDir = '/home/wwwsync/input/'
 
-'''
-
-banList = '/home/wwwsync/input/banlist.txt'
+lockFile = 'disable.txt'
+unlockFile = 'enable_aacount.txt'
 
 
-for user in file(banList).readline():
-	## Chk usr whether is online
-	pid = os.popen('who -u | grep '+user+' | tr -s " " | cut -d " " -f6 ').read()
-	os.system('kill -9 '+pid)
+for user in file(lockFile).readline():
+        ## Chk usr whether is online
+        pid = os.popen('who -u | grep '+user+' | tr -s " " | cut -d " " -f6 ').read()
+        os.system('kill -9 '+pid)
 
-	## Make Usr account expired
-	ssh = subprocess.Popen(['ssh', 'master', 'chage -E 0'], shell=False, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        ## Make Usr account expired
+        ssh = subprocess.Popen(['ssh', 'master', 'chage -E 0'+user], shell=False, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
-	## Rename Usr's authorized_keys in /home/USER/.ssh
-	ssh = subprocess.Popen(['ssh', 'master', 'mv /home/'+user+'/.ssh/authorized_keys /home/'+user+'/.ssh/expire_keys'])
+        ## Rename Usr's authorized_keys in /home/USER/.ssh
+        ssh = subprocess.Popen(['ssh', 'master', 'mv /home/'+user+'/.ssh/authorized_keys /home/'+user+'/.ssh/expire_keys'])
