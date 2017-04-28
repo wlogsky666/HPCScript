189d222663879b603bc94472045f859851efe0b4
Wlogsky <wlogsky666@gmail.com>
2017-04-29 00:18:36 +0800

    grep -w

--- expireUser.py
+++ expireUser.py
@@ -11,30 +11,25 @@ lockFile = 'disable.txt'
 unlockFile = 'enable_aacount.txt'
 
 
-# Lock User
+# Lock Usr
 for user in file(lockFile).readlines():
-        ## Chk usr whether is online
-        cmd = 'who -u | tr -s " " | cut -d " " -f1,6'
-        rst = os.popen(cmd).read()
-        name = rst.split(' ')[0]
-        pid = rst.split(' ')[1]
+         ## Chk usr whether is online
+        pid = os.popen('who -u | grep -w '+user.strip()+' | tr -s " " | cut -d " " -f6 ').read()
 
-        if name == user.strip(): 
-	        print(name, pid)
-	        os.system('kill -9 '+pid)
+        # Kick Usr
+        if len(pid) > 0 :
+                os.system('kill -9 '+pid)
 
         ## Make Usr account expired
-        ssh = subprocess.Popen(['ssh', 'master', 'chage -E 0 '+name], shell=False, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        ssh = subprocess.Popen(['ssh', 'master', 'chage -E 0 '+user.strip()], shell=False, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
         ## Rename Usr's authorized_keys in /home/USER/.ssh
-        os.popen('mv /home/'+name+'/.ssh/authorized_keys /home/'+name+'/.ssh/expire_keys' )
+        os.popen('mv /home/'+user.strip()+'/.ssh/authorized_keys /home/'+user.strip()+'/.ssh/expire_keys' )
 
-
-
+# Unlock Usr
 for user in file(unlockFile).readlines():
-
-		## Make Usr account active
-        ssh = subprocess.Popen(['ssh', 'master', 'chage -E -1 '+name], shell=False, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        ## Make Usr account active
+        ssh = subprocess.Popen(['ssh', 'master', 'chage -E -1 '+user.strip()], shell=False, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
         ## Rename Usr's authorized_keys in /home/USER/.ssh
-        os.popen('mv /home/'+name+'/.ssh/expire_keys /home/'+name+'/.authorized_keys' )
+        os.popen('mv /home/'+user.strip()+'/.ssh/expire_keys /home/'+user.strip()+'/.authorized_keys' )
