d6e18e5ca987d990a1c16800e8ca1e71cdd80b00
Wlogsky <wlogsky666@gmail.com>
2017-05-09 05:04:59 +0800

    Update expireUser.py

--- expireUser.py
+++ expireUser.py
@@ -2,34 +2,74 @@
 
 import os
 import subprocess
+import time
+import datetime
+
+## Debug Option
+debug = False
+lock = False
 
 ## Get rsync file of account list at /home/wwwsync/input/
 
 homeDir = '/home/wwwsync/input/'
+lockFile = 'disable_account.txt'
+unlockFile = 'enable_account.txt'
 
-lockFile = 'disable.txt'
-unlockFile = 'enable_aacount.txt'
+if debug:
+        homeDir = './'
+        print 'Debug Mode...'
+        if lock:
+                lockFile, unlockFile = unlockFile, lockFile
 
+ima = datetime.datetime.now().strftime("%Y%m%d-%H%M")
+log = file('./log/'+datetime.datetime.now().strftime("%Y%m%d"), 'a')
 
+log.write(ima)
+log.write('\n[Lock User]')
 # Lock Usr
-for user in file(lockFile).readlines():
-         ## Chk usr whether is online
-        pid = os.popen('who -u | grep -w '+user.strip()+' | tr -s " " | cut -d " " -f6 ').read()
+for user in file(homeDir+lockFile).readlines():
+        if len(user.strip()) == 0 : continue ;
+        ## Chk user has already been locked
+        s = os.popen('ssh master "passwd -S '+user.strip()+' | cut -d\' \' -f9"')
+        if s.readlines()[0].startswith('locked.') : continue ;
 
-        # Kick Usr
-        if len(pid) > 0 :
-                os.system('kill -9 '+pid)
+        log.write( user.strip())
+        ## Chk usr whether is online
+        os.system('pkill -9 -u '+user.strip())
+        print 'pkill -9 -u '+user.strip()
 
         ## Make Usr account expired
-        ssh = subprocess.Popen(['ssh', 'master', 'chage -E 0 '+user.strip()], shell=False, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        ssh = subprocess.Popen(['ssh', 'master', 'passwd --lock '+user.strip()], shell=False, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
         ## Rename Usr's authorized_keys in /home/USER/.ssh
-        os.popen('mv /home/'+user.strip()+'/.ssh/authorized_keys /home/'+user.strip()+'/.ssh/expire_keys' )
+        os.system('mv /home/'+user.strip()+'/.ssh/authorized_keys /home/'+user.strip()+'/.ssh/xauthorized_keys')
 
+
+log.write('\n[Unlock User]')
 # Unlock Usr
-for user in file(unlockFile).readlines():
-        ## Make Usr account active
-        ssh = subprocess.Popen(['ssh', 'master', 'chage -E -1 '+user.strip()], shell=False, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+for user in file(homeDir+unlockFile).readlines():
+        if len(user.strip()) == 0 : continue;
+        ## Chk user has already benn unlocked
+        s = os.popen('ssh master "passwd -S '+user.strip()+' | cut -d\' \' -f9"')
+        if s.readlines()[0].startswith('set') : continue ;
 
-        ## Rename Usr's authorized_keys in /home/USER/.ssh
-        os.popen('mv /home/'+user.strip()+'/.ssh/expire_keys /home/'+user.strip()+'/.authorized_keys' )
+        log.wrtie(user.strip())
+        ## Make Usr account active
+        ssh = subprocess.Popen(['ssh', 'master', 'passwd --unlock '+user.strip()], shell=False, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+                 ## Rename Usr's authorized_keys in /home/USER/.ssh
+        os.popen('mv /home/'+user.strip()+'/.ssh/xauthorized_keys /home/'+user.strip()+'/.ssh/authorized_keys' )
+
+
+# wait until passwd refresh
+time.sleep(3)
+log.write('\n[/var/yp]')
+
+# yp
+yp = subprocess.Popen(['ssh', 'master', 'make -C /var/yp'], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+out = yp.stdout.readlines()
+for r in out:
+        log.write(r)
+
+         
