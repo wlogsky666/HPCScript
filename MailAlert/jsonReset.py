898f2310e74d6c3cbd7e624339b8dd0bcb036c9e
Wlogsky <wlogsky666@gmail.com>
2017-05-18 03:07:57 +0800

    Update jsonReset.py

--- MailAlert/jsonReset.py
+++ MailAlert/jsonReset.py
@@ -2,15 +2,11 @@ import json
 import os
 
 Node = { node.strip() for node in os.popen('pbsnodes -l all | cut -d" " -f1')}
+Node.update(['oss1', 'oss2', 'mds'])
 NodeTemp = dict()
 
 for n in Node:
         NodeTemp[n] = 0
-
-NodeTemp['oss1'] = 0
-NodeTemp['oss2'] = 0
-NodeTemp['mds']  = 0
-
 
 with open('SendTimes.json', 'w') as st:
         json.dump(NodeTemp, st)
